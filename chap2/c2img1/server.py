1468807d558a63e8c9e86a4b14e50af12d9403aa
mrjrk-kojima <kojima.michito@social-sharing.org>
2021-10-24 15:44:00 +0900

    before jenkins

--- chap2/c2img1/server.py
+++
@@ -1,8 +0,0 @@
-import flask
-app = flask.Flask('app server')
-
-@app.route('/')
-def index():
-  return 'Hello Docker'
-
-app.run(debug=True, host='0.0.0.0', port=80)
--- chap6/c6cicd/README.md
+++
@@ -1,3 +0,0 @@
-オリジナルのGitHubリポジトリは以下となります。
-
-https://github.com/yuichi110/docker-kvs
